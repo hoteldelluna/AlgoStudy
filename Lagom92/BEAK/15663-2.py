4fab86570abed9addc273fb521f875249f4d2b88
Yang Siyeong <jyyt0147@gmail.com>
2019-09-02 14:24:35 +0900

    EXPERT | 5189

--- Lagom92/BEAK/15663-2.py
+++ Lagom92/BEAK/15663-2.py
@@ -8,28 +8,7 @@ N개의 수
 출력:
 N개의 자연수 중에서 M개의 수열(수열이 중복 X)
 
+
+나중에 할 예정
 '''
 
-
-def f(n, m, k):
-    if n == k:
-        print(res)
-        result.append(res)
-        return
-
-    for i in range(m):
-        if not used[i]:
-            used[i] = 1
-            res[n] = arr[i]
-            f(n+1, m, k)
-            used[i] = 0
-
-N, M = map(int, input().split())
-arr = list(sorted(map(int, input().split())))
-
-used = [0]*N
-res = [0]*M
-
-result = []
-f(0, N, M)
-print(result)
